3886ebc85133bfa95534ebef534b4fcb30aa0eaf
Ewerson <ewersonv@gmail.com>
2020-06-13 22:14:56 -0300

    add dateInputMask

--- src/components/EditProfileModal/index.js
+++ src/components/EditProfileModal/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FaAt, FaInstagram, FaTimes, FaTwitter } from 'react-icons/fa';
 
@@ -35,6 +36,39 @@ function EditProfileModal() {
   function onEscDown(e) {
     if (e.key === 'Escape') closeModal(e);
   }
+
+  function dateInputMask(elm) {
+    elm.addEventListener('keypress', (e) => {
+      if (e.keyCode < 47 || e.keyCode > 57) {
+        e.preventDefault();
+      }
+
+      const len = elm.value.length;
+
+      // If we're at a particular place, let the user type the slash
+      // i.e., 12/12/1212
+      if (len !== 1 || len !== 3) {
+        if (e.keyCode === 47) {
+          e.preventDefault();
+        }
+      }
+
+      // If they don't add the slash, do it for them...
+      if (len === 2) {
+        elm.value += '/';
+      }
+
+      // If they don't add the slash, do it for them...
+      if (len === 5) {
+        elm.value += '/';
+      }
+    });
+  }
+
+  useEffect(() => {
+    const input = document.getElementById('js-date');
+    dateInputMask(input);
+  });
 
   document.addEventListener('keydown', onEscDown);
 
@@ -95,7 +129,12 @@ function EditProfileModal() {
           </InputField>
           <InputField>
             <span className="input-label">Data de nascimento</span>
-            <input type="text" />
+            <input
+              type="text"
+              maxLength="10"
+              id="js-date"
+              placeholder="DD/MM/AAAA"
+            />
           </InputField>
           <LogoutButton
             onClick={() => {
